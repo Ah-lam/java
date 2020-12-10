e6c2c0be6fd7ef4623e064adf0cfa37f1443cadb
Ah-lam <187318190@qq.com>
2020-12-10 14:23:05 +0800

    FunctionalInterfaceExample commit

---
+++ src/main/java/com/predicate/Hosting.java
@@ -0,0 +1,13 @@
+package com.predicate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Hosting {
+
+    private int Id;
+    private String name;
+    private String url;
+}
